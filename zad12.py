b9e96282d2c5c5e2b7b6d6576a97b970cd9434d3
oOska4 <102377519+oOska4@users.noreply.github.com>
2025-11-29 16:53:32 +0100

    Create zad12.py

---
+++ zad12.py
@@ -0,0 +1,14 @@
+def na_dec(x, s):
+    x = str(x)
+    wynik = 0
+    for cyfra in x:
+        wynik = wynik * s + int(cyfra)
+    return wynik
+
+def main():
+    x = input("Podaj liczbę w systemie s: ")
+    s = int(input("Podaj system (2–9): "))
+    print("Wynik:", na_dec(x, s))
+
+if __name__ == "__main__":
+    main()
